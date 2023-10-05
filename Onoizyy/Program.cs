20b23f836c7ad2221492dcdd07466fa3691c8ef0
Onoizyy <izianyonorio@gmail.com>
2023-10-05 15:08:02 -0400

    Primeiro commit

---
+++ Onoizyy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; 
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Diggite um numero:");
+        double num = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite outro numero:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        double resultad = num + num2;
+        Console.WriteLine($"A soma dos numeros é {resultad}");
+        Console.ReadKey();
+
+    }
+}
